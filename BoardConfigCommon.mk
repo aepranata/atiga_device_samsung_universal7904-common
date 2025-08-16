b743e29cf12b12a6994c70961d39c5526193e015
Aditya Eka Pranata <aepranata@gmail.com>
2025-08-16 09:26:50 +0700

    universal7904: Compile with `Atiga Clang`
    Change-Id: I1d5b9ce22ce8192369035b5b3b3e37c7f72bf24c
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,6 +48,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/samsung/universal7904
 TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := atiga
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
